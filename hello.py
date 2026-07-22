a6ba2e57abc2dce64ccfb93f73f4240b5760a698
R-Mahin <riduanislam425@gmail.com>
2026-07-22 12:51:43 +0600

    riduan func added

--- hello.py
+++ hello.py
@@ -4,10 +4,15 @@ def main():
 def Zunaid():
     print("Git is Cool")
 
+def riduan():
+    print("riduan is here")
+
 main()
 Zunaid()
 def hello_77():
     print('Hello from mahir bhai')
+
+riduan()
 
 main()
 hello_77()
